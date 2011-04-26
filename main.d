171c4fa029540a5df1c3d1a613dc3570343427af
Ben Foppa <benjamin.foppa@gmail.com>
2011-04-25 21:13:01 -0400

    Started fleshing out more of the program.

--- main.d
+++ main.d
@@ -1,3 +1,21 @@
+import std.stdio, std.file, std.stream;
+import vocab_tester;
+
 void main(string[] args)
 {
+    if(args.length <= 1)
+    {
+        writeln("No input file given. Quitting.");
+        return;
+    }
+
+    auto filename = args[1];
+
+    if(!exists(filename))
+    {
+        writeln("Input file doesn't exist. Quitting.");
+        return;
+    }
+
+    vocabTest(new BufferedFile(filename));
 }
---
+++ vocab_tester.d
@@ -0,0 +1,52 @@
+import std.stream, std.string, std.container;
+
+private
+{
+    static struct DictionaryEntry
+    {
+        string term;
+        string definition;
+    }
+
+    //TODO: Pick a more efficient data structure.
+    alias DictionaryEntry[] Dictionary;
+
+    immutable seperator = " | ";
+
+    //Undefined behaviour for invalid lines.
+    private DictionaryEntry parseLine(string line)
+    {
+        assert(isValidLine(line));
+
+        const seperatorIndex = indexOf(line, seperator);
+        assert(seperatorIndex != -1);
+
+        DictionaryEntry ret = { line[0..seperatorIndex], line[seperatorIndex + seperator.length..$] };
+        return ret;
+    }
+
+    //Returns true iff [line] defines a valid dictionary entry.
+    bool isValidLine(string line)
+    {
+        return indexOf(line, seperator) != -1;
+    }
+
+    Dictionary parseDictionary(BufferedStream input)
+    {
+        Dictionary dictionary;
+
+        foreach(char[] line; input)
+        {
+            const string strLine = line.idup;
+            if(isValidLine(strLine))
+                dictionary ~= parseLine(strLine);
+        }
+
+        return dictionary;
+    }
+}
+
+void vocabTest(BufferedStream input)
+{
+    assert(false, "Unimplemented.");
+}
